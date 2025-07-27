874a4ffc475d2fd2911e974f6e6bd6fcc472e73d
Manoj Hembram <manoj03h@gmail.com>
2025-07-27 20:36:57 +0530

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+import "./ui/global.css"
+import { inter } from '@/app/ui/fonts';
+
+const RootLayout = ({children}:{children: React.ReactNode}) => {
+  return (
+    <html lang="en">
+       <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  )
+}
+
+export default RootLayout;
